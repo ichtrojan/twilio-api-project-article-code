8f931ae225f785e9b62ca7aa6d4aca39c68a23d9
Michael okoh <ordrizzy@gmail.com>
2019-02-21 22:06:58 +0100

    get student test

--- tests/Unit/ApiTest.php
+++ tests/Unit/ApiTest.php
@@ -5,17 +5,27 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Student;
 
 class ApiTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
     public function testGetStudents()
     {
-      $response = $this->get('/api/students');
-      $response->assertStatus(200);
+        $response = $this->get('/api/students');
+        $response->assertStatus(200);
+    }
+
+    public function testGetStudent()
+    {
+        // $student = factory(\App\Student::class)->create();
+        $response = $this->get('/api/student/1');
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+          [
+            'id',
+            'name',
+            'course',
+          ]
+        ]);
     }
 }
